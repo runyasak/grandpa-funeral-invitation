cfec86800b7a8d8306a0a63e638be146774e333f
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2025-07-21 01:25:35 +0700

    Update index.html and App.vue for improved layout and styling; enhance main.css with theme variables

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="container mx-auto p-4">
-    <h1 class="text-2xl font-bold mb-4">Welcome to My Vue App</h1>
-    <p class="text-gray-700">This is a simple Vue.js application.</p>
+  <div id="app" class="bg-primary h-screen">
+    <div class="container mx-auto h-full p-4 flex flex-col justify-center items-center text-center">
+      <div class="max-w-xs rounded overflow-hidden shadow-lg mb-8">
+        <img alt="Grandpa Image"
+          src="https://firebasestorage.googleapis.com/v0/b/grandpa-funeral-invitation.firebasestorage.app/o/IMG_1519.JPG?alt=media&token=be64224a-9306-49e8-baae-69462c2eff69">
+      </div>
+
+      <p class="text-2xl text-white mb-4">ขอเชิญร่วมรับฟังสวดพระอภิธรรมศพ</p>
+      <p class="text-2xl text-white mb-4">คุณพ่อ ย้อย แจ้งในเมือง</p>
+
+      <p class="text-xl text-white mb-8">ณ วัดสรรเพชญ อำเภอสามพราน จังหวัดนครปฐม</p>
+
+      <button class="border border-white text-white font-bold py-2 px-4 rounded-full mb-4 cursor-pointer">
+        สร้างบัตรเชิญร่วมงานผ่าน LINE
+      </button>
+    </div>
   </div>
 </template>
